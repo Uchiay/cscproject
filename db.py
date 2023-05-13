1e7b8c4ada58333b16ba58e88254a229de594139
Puneet Aggarwal <puneetaggarwal@Puneets-MacBook-Pro.local>
2023-05-13 15:14:19 -0400

    changes

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ def get_connection():
     mypass = "sasuke123"
     mydatabase="bookmanagementsystem"
 
-    con = pymysql.connect(host="bookmanagementsystem.c9gdd4dr2imu.us-east-1.rds.amazonaws.com",user="root",password=mypass,database=mydatabase)
+    con = pymysql.connect(host="bookmanagementsystem.c9gdd4dr2imu.us-east-1.rds.amazonaws.com",user="admin",password=mypass,database=mydatabase)
 
     return con
     
